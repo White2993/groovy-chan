0ac0b089b5c63e50bcb97c943e85f5970ca5eca1
White2993 <47195867+White2993@users.noreply.github.com>
2020-08-14 23:07:55 -0500

    Added the ability for user to add/change username

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,26 @@
-function userNamePrompt() {
-  var userName = prompt("Please Enter a Username");
-}
-
 function homePageDisplay() {
   var userName = prompt("Please Enter a Username");
   document.getElementById("userNameDisplay").innerHTML = "Welcome, " + userName + "!"
   if(!userName) {
     document.getElementById("userNameDisplay").innerHTML = "Howdy, Pardner!"
   }
+  localStorage.setItem('username', userName);
+  console.log(localStorage.getItem('username'))
+}
+
+function userNameCheck() {
+  if (localStorage.getItem('username') === "" || localStorage.getItem('username') === null) {
+    homePageDisplay();
+  } else {
+    document.getElementById("userNameDisplay").innerHTML = "Welcome, " + localStorage.getItem('username') + "!"
+  }
+}
+
+function profilePageDisplay() {
+  document.getElementById("profileNameDisplay").innerHTML = "Congratulations, " + localStorage.getItem('username')
+}
+
+function userNameClear() {
+  localStorage.clear('username');
+  location.reload();
 }
